f8e7c2f73ab0cdf5767ac2100a4be6650000c354
timeyyy <timeyyy_da_man@hotmail.com>
2016-02-29 20:36:56 +0100

    update readme and version bump 0.1.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ REQUIRED = []
 setup(
 	name = 'system_hotkey',
 
-	version='0.1.3',
+	version='0.1.4',
 	
 	description = 'System wide hotkeys',
 	long_description = (read('README.rst') + '\n\n' +
